b905c9c63f52a996322e64a6493c8b896aa24757
구동혁 <dhku@naver.com>
2025-04-26 16:22:03 +0900

    feat: 초기 모듈 프로젝트 환경 셋팅

---
+++ ssok-example/src/test/java/kr/ssok/example/SsokExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ssok.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsokExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
