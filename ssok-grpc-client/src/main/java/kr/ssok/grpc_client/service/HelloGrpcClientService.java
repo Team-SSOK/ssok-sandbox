b1957ff6c01b0d9c7d1862360c7e9f7e802b5579
isasgp <isasgp@naver.com>
2025-05-07 09:23:37 +0900

    feat: protobuf 파일에 optional 추가

--- ssok-grpc-client/src/main/java/kr/ssok/grpc_client/service/HelloGrpcClientService.java
+++ ssok-grpc-client/src/main/java/kr/ssok/grpc_client/service/HelloGrpcClientService.java
@@ -22,6 +22,11 @@ public class HelloGrpcClientService {
             // 요청에 대한 결과 받기
             // 내가(클라이언트가) 가진 메소드가 아닌, 서버가 가진 메소드를 사용
             HelloReply response = greeterBlockingStub.sayHello(request);
+            // 이 부분이 protobuf에서 optional로 선언했기 때문에, 이런 식으로 구현 가능
+            // repeated는 isEmpty로 판별
+            if(response.hasMessage()) {
+                log.info(response.getMessage());
+            } // 이 부분에 protobuf에서 optional 했으니까 제대로 왔는지 확인하고 싶음
             return response.getMessage();
         } catch (StatusRuntimeException e) {
             log.warn("gRPC request failed: {}", e.getStatus(), e);
--- ssok-grpc-server/src/main/java/kr/ssok/grpc_server/service/GreeterService.java
+++ ssok-grpc-server/src/main/java/kr/ssok/grpc_server/service/GreeterService.java
@@ -1,5 +1,6 @@
 package kr.ssok.grpc_server.service;
 
+import io.grpc.Status;
 import io.grpc.examples.helloworld.GreeterGrpc;
 import io.grpc.examples.helloworld.HelloReply;
 import io.grpc.examples.helloworld.HelloRequest;
@@ -10,6 +11,16 @@ import org.springframework.stereotype.Service;
 public class GreeterService extends GreeterGrpc.GreeterImplBase {
     @Override
     public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+        // 이 부분이 protobuf에서 optional로 선언했기 때문에, 이런 식으로 구현 가능
+        // repeated는 isEmpty로 판별
+        if (!req.hasName()) {
+            // 클라이언트가 name 필드를 아예 안 보낸 경우
+            responseObserver.onError(Status.INVALID_ARGUMENT
+                    .withDescription("Name is required.")
+                    .asRuntimeException());
+            return;
+        }
+
         HelloReply reply = HelloReply.newBuilder()
                 .setMessage("Hello " + req.getName())
                 .build();
